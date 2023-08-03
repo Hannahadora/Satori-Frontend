a7aab4616fdf9dd8045b1e1ca7d83fc3650d0d55
hannah <hannahadora97@gmail.com>
2023-08-03 17:46:40 +0100

    update on index script

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,16 @@
 import React from "react";
-// import ReactDOM from "react-dom/client";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-//   <React.StrictMode>
-//     <ToastContainer toastStyle={{ border: "1px solid #635FC7" }} />
-//     <App />
-//   </React.StrictMode>
-// );
-
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ToastContainer toastStyle={{ border: "1px solid #635FC7" }} />
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
