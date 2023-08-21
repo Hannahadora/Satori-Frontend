25daeb2baab635ede613811ff45c8f8b9e980343
Nanagabby95 <nancitonana86@gmail.com>
2023-08-21 20:48:31 +0100

    cookies

---
+++ src/pages/cookies.tsx
@@ -0,0 +1,28 @@
+
+const cookies=()=>{
+    return(
+        <div className="mt-[130px]">
+            <div className="app-container">
+                 <div className="lg:w-[70%] md-[60%] w-full mx-auto">
+                    <h3 className="lg:text-[56px] md:text-[45px] text-[35px] lg:leading-p[70px] font-[700] text-[#010809] text-center">Satori Cookie Policy</h3>
+                    <p className="lg:text-[18px] md:text-[14px] text-[12px] text-center pt-3">Effective Date: 30th September, 2023</p>
+                    <p className="lg:text-[22px] md:text-[18px] text-[16px] text-center pt-4">This Cookie Policy explains how we use cookies and similar tracking technologies on our website. By using the Website, you consent to the use of cookies as described in this policy.</p>
+                    <h3 className="lg:text-[26px] md:text-[20px] text-[16px] text-center pt-14">What are cookies?</h3>
+                    <p className="lg:text-[22px] md:text-[18px] text-[16px] text-center pt-4">Cookies are small pieces of data stored on your device when you visit our Website. They help improve your experience by remembering your preferences, analyzing how you use the site, and providing personalized content.</p>
+                    <h3 className="lg:text-[26px] md:text-[20px] text-[16px] text-center pt-14">How cookies are used on Satori</h3>
+                    <p className="lg:text-[22px] md:text-[18px] text-[16px] text-center pt-4">We use cookies for various purposes including:</p>
+                    <p className="lg:text-[22px] md:text-[18px] text-[16px] text-center pt-4"><b>Essential Cookies:</b>These cookies are necessary for the proper functioning of the Website. They enable you to navigate the site and use its features.</p>
+                    <p className="lg:text-[22px] md:text-[18px] text-[16px] text-center pt-4"><b>Performance Cookies:</b>These cookies collect anonymous information about how visitors use our Website. They help us analyze and improve the performance of our services.</p>
+                    <p className="lg:text-[22px] md:text-[18px] text-[16px] text-center pt-4"><b>Third-party Cookies:</b>Our Website may use third-party services that place cookies on your device. These cookies are subject to the respective privacy policies of these external services providers.</p>
+                    <h3 className="lg:text-[26px] md:text-[20px] text-[16px] text-center pt-14">Managing Cookies</h3>
+                    <p className="lg:text-[22px] md:text-[18px] text-[16px] text-center pt-4">You can control and manage cookies in your browser settings. You can choose to block or delete cookies, but some features of the Website may not function correctly if you do so</p>
+                    <h3 className="lg:text-[26px] md:text-[20px] text-[16px] text-center pt-14">Your Consent</h3>
+                    <p className="lg:text-[22px] md:text-[18px] text-[16px] text-center py-4">If you have any questions or concerns about our Cookie Policy, please reach out to us at hello@satori.ng</p>
+                 </div>
+
+            </div>
+            
+        </div>
+    )
+}
+export default cookies
