2edc714325156dcc655d9548b3469908bcff019b
hannah <hannahadora97@gmail.com>
2023-09-07 20:22:12 +0100

    update on index page

--- src/components/Home/Faq.tsx
+++ src/components/Home/Faq.tsx
@@ -1,14 +1,17 @@
 import { useState, useEffect } from "react";
-import { IFaq, faqsData } from "../../../public/json/faqsData";
+import faqsData from "../FAQcomponents/FaqData";
+import communityFaqData from "../CommunityCompo/CommunityFaqData";
 import arrow_up from "../../assets/icons/circle_arrow_up.svg";
 import arrow_down from "../../assets/icons/circle_arrow_down.svg";
+import { Link } from "react-router-dom";
 
 const Faq = () => {
-  const [faqs, setFaqs] = useState(faqsData);
+  const [faqs, setFaqs] = useState(faqsData.slice(0,3));
+  const [commuunityFaqs, setCommunityFaqs] = useState(communityFaqData.slice(0,3));
   const [activeBtn, setActiveBtn] = useState('satori') 
 
   // Use an array of boolean states to manage whether each FAQ's answer is shown or not
-  const [showAnswer, setShowAnswer] = useState(new Array(faqs.length).fill(false));
+  const [showAnswer, setShowAnswer] = useState(new Array(activeBtn === 'satori' ? faqs.length : commuunityFaqs.length).fill(false));
 
 
   const toggleShowAnswer = (index: any) => {
@@ -19,12 +22,13 @@ const Faq = () => {
   };
 
   useEffect(() => {
-    setFaqs(faqsData)
+    setFaqs(faqsData.slice(0,3))
+    setCommunityFaqs(communityFaqData.slice(0,3))
   }, [])
   
 
   return (
-    <div className="app-container pt-[80px]">
+    <div className="app-container pt-[80px] pb-[40px]">
       <div className="md:px-[24px] px-[16px] pt-[24px] pb-[40px] bg-[#F5E8D7] rounded-[32px]">
         <div className="lg:w-[30%] md:w-[70%] w-[100%] mb-[30px] mx-auto text-center">
           <h5 className="text-[#021012] md:text-[20px] text-[18px]">
@@ -40,21 +44,39 @@ const Faq = () => {
                 <p onClick={() => setActiveBtn('satori')} className={`text-[#353B3C] md:px-[16px] px-[10px] py-[8px] md:text-[18px] rounded-[100px] cursor-pointer ${activeBtn === 'satori' && 'border border-[#353B3C]'}`}>About Satori</p>
                 <p onClick={() => setActiveBtn('community')} className={`text-[#353B3C] md:px-[16px] px-[10px] py-[8px] md:text-[18px] rounded-[100px] cursor-pointer ${activeBtn === 'community' && 'border border-[#353B3C]'}`}>About our community</p>
             </div>
-          {faqs &&
-            faqs.map((faq: IFaq, i: any) => (
+          {activeBtn === 'satori' &&
+            faqs.map((faq: any, i: any) => (
               <div key={i} className="md:p-[16px] mt-[16px] p-[5px] border-b border-[#808C8E]">
                 <div className="flex items-start justify-between">
                   <div className="mr-[8px] md:w-auto w-[60%]">
-                    <p className="md:text-[20px] text-[16px]">{faq.question}</p>
+                    <p className="md:text-[20px] text-[16px] font-[600]">{faq.question}</p>
                   </div>
                   <div className="md:w-auto w-[40%] flex items-center justify-end">
-                    {!showAnswer[i] && (
-                      <h5 className="md:text-[20px] text-[16px] mr-[2px]">{faq.openings}</h5>
-                    )}
                     <img
                     onClick={() => toggleShowAnswer(i)}
                     className="cursor-pointer"
-                      src={showAnswer[i] ? arrow_down : arrow_up}
+                      src={showAnswer[i] ? arrow_up : arrow_down}
+                      alt="arrow"
+                    />
+                  </div>
+                </div>
+                {showAnswer[i] && (
+                  <p className="mt-[10px] text-[#353B3C]">{faq.answer}</p>
+                )}
+              </div>
+            ))}
+          {activeBtn === 'community' &&
+            commuunityFaqs.map((faq: any, i: any) => (
+              <div key={i} className="md:p-[16px] mt-[16px] p-[5px] border-b border-[#808C8E]">
+                <div className="flex items-start justify-between">
+                  <div className="mr-[8px] md:w-auto w-[60%]">
+                    <p className="md:text-[20px] text-[16px] font-[600]">{faq.question}</p>
+                  </div>
+                  <div className="md:w-auto w-[40%] flex items-center justify-end">
+                    <img
+                    onClick={() => toggleShowAnswer(i)}
+                    className="cursor-pointer"
+                      src={showAnswer[i] ? arrow_up : arrow_down}
                       alt="arrow"
                     />
                   </div>
@@ -65,6 +87,13 @@ const Faq = () => {
               </div>
             ))}
         </div>
+
+        
+      <div className="flex flex-col items-center justify-center m-[24px]">
+        <button className="sec-btn text-[18px] md:px-[32px] px-[20px] py-[8px] rounded-[100px]">
+          <Link to={'/faqs'}>See more FAQs</Link>
+        </button>
+      </div>
       </div>
     </div>
   );
